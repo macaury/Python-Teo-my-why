6bedf86618a266c31b684d5f1337f3623770c8aa
macaury <carvalhomacaury@gmail.com>
2025-08-14 13:46:20 -0300

    aprendendo gostosin

--- atividades/02.py
+++ atividades/02.py
@@ -37,69 +37,29 @@ df_clientes["vinculo_social_media"] = (df_clientes['FlTwitch']==1) +(df_clientes
 
 df_clientes[df_clientes["vinculo_social_media"]]
 
-
-
-
 #%%
-
 
 # Qual é o id de cliente que tem maior saldo de pontos? E o menor?
 
-
 maior_pontuador = df_clientes["QtdePontos"].max()
-
 maior_pontuador = df_clientes["QtdePontos"] == maior_pontuador
-
 df_clientes[maior_pontuador] ## Cliente com mais pontos
-
 menor_pontuador = df_clientes["QtdePontos"] == 1
-
 df_clientes[menor_pontuador]
 
 
 #%%
 
 df_transacoes = pd.read_csv("../data/transacoes.csv",sep=";")
-
-df_transacoes.head()
-
-#%%
-
 df_transacoes["DtCriacao"] = df_transacoes["DtCriacao"].str.replace(" +0000 UTC",'')
 
+df_transacoes["Data_criacao"] = (pd.to_datetime(
+    df_transacoes["DtCriacao"], format='mixed',utc=True)).dt.date
 
-#df_transacoes["DtCriacao"]
-#%%
+df_transacoes["Data_criacao"]
 
-# convertendo a coluna data para o formato datetime
-
-df_transacoes["DtCriacao"] = pd.to_datetime(
-    df_transacoes["DtCriacao"], format='mixed',utc=True)
-
-
-df_transacoes["DtCriacao"]
-
-# %%
-
-#ordernando o Dataframe pelo id do cliente e dia da transação
-
-df_transacoes = df_transacoes.sort_values(by=["IdCliente","DtCriacao"],ascending=False)
-
-#df_transacoes
-
-#%%
-
-# Coluna somente com a data, sem a hora para poder filtrar pelo dia
-
-df_transacoes["data_clean"] = df_transacoes["DtCriacao"].dt.date
-
-df_transacoes["data_clean"]
-
-
-
-# %%
-# remover as transações duplicada , com base nad ata da transação
-
-duplicados = df_transacoes.drop_duplicates(keep="first",subset="data_clean")
+df_transacoes = df_transacoes.sort_values(by=["IdCliente","Data_criacao"],ascending=False) 
+duplicados = df_transacoes.drop_duplicates(keep="first",subset=["IdCliente","Data_criacao"])
 
 duplicados
+# %%
